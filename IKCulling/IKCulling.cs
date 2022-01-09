847496c959f7364a35577c64fb33800bb0c2d8d1
Kyuubi <kyuubi.no.yoko86@gmail.com>
2022-01-09 21:58:18 +0100

    Update to ModConfiguration Some cleanup

--- IKCulling/IKCulling.cs
+++ IKCulling/IKCulling.cs
@@ -5,6 +5,7 @@ using NeosModLoader;
 using BaseX;
 using FrooxEngine;
 using FrooxEngine.FinalIK;
+using Leap.Unity;
 
 
 namespace IkCulling
@@ -16,64 +17,109 @@ namespace IkCulling
         public override string Version => "1.1.0";
         public override string Link => "https://github.com/KyuubiYoru/IkCulling";
 
-        internal static ModConfiguration Config;
+        public static ModConfiguration Config;
 
-        internal static readonly ModConfigurationKey<bool> Enabled =
+        public static readonly ModConfigurationKey<bool> ConfigFileExist =
+            new ModConfigurationKey<bool>("ConfigFileExist",
+                "Value to check if the config file need to be initialized.", () => false, true);
+
+        public static readonly ModConfigurationKey<bool> Enabled =
             new ModConfigurationKey<bool>("Enabled", "IkCulling Enabled.", () => true);
 
-        internal static readonly ModConfigurationKey<bool> UseUserScale =
+        public static readonly ModConfigurationKey<bool> UseUserScale =
             new ModConfigurationKey<bool>("UseUserScale", "Should user scale be used for Distance check.", () => false);
 
-        internal static readonly ModConfigurationKey<float> Fov = new ModConfigurationKey<float>("Fov",
+        public static readonly ModConfigurationKey<float> Fov = new ModConfigurationKey<float>("Fov",
             "Field of view used for IkCulling, can be between 1 and -1.",
             () => 0.7f, false, v => v <= 1f && v >= -1f);
 
-        internal static readonly ModConfigurationKey<float> MinRange =
-            new ModConfigurationKey<float>("_minRange", "Minimal range for IkCulling, useful in front of a mirror.",
+        public static readonly ModConfigurationKey<float> MinCullingRange =
+            new ModConfigurationKey<float>("MinCullingRange",
+                "Minimal range for IkCulling, useful in front of a mirror.",
                 () => 4);
 
-        internal static readonly ModConfigurationKey<float> MaxViewRange =
-            new ModConfigurationKey<float>("_maxViewRange", "Maximal view range where IkCulling is always enabled.",
+        public static readonly ModConfigurationKey<float> MaxViewRange =
+            new ModConfigurationKey<float>("MaxViewRange", "Maximal view range where IkCulling is always enabled.",
                 () => 30);
+
+        private static bool _enabled = true;
+        private static bool _useUserScale = false;
+        private static float _fov = 0.7f;
+        private static float _minCullingRange = 4;
+        private static float _maxViewRange = 30;
 
 
         public override ModConfigurationDefinition GetConfigurationDefinition()
         {
-            List<ModConfigurationKey> keys = new List<ModConfigurationKey>();
-            keys.Add(Enabled);
-            keys.Add(Fov);
-            keys.Add(MinRange);
-            keys.Add(MaxViewRange);
+            try
+            {
+                List<ModConfigurationKey> keys = new List<ModConfigurationKey>();
+                keys.Add(ConfigFileExist);
+                keys.Add(Enabled);
+                keys.Add(UseUserScale);
+                keys.Add(Fov);
+                keys.Add(MinCullingRange);
+                keys.Add(MaxViewRange);
 
-            return DefineConfiguration(new Version(1, 0, 0), keys);
+                return DefineConfiguration(new Version(1, 0, 0), keys);
+            }
+            catch (Exception e)
+            {
+                Error(e.Message);
+                Error(e.StackTrace);
+                throw;
+            }
         }
 
         public override void OnEngineInit()
         {
-            Config = GetConfiguration();
+            try
+            {
+                Harmony harmony = new Harmony("net.kyuubiyoru.IkCulling");
+                harmony.PatchAll();
 
-            Harmony harmony = new Harmony("net.KyuubiYoru.IkCulling");
-            harmony.PatchAll();
+                Config = GetConfiguration();
+                Config.OnThisConfigurationChanged += RefreshConfigState;
+
+                if (!Config.GetValue(ConfigFileExist))
+                {
+                    Config.Set(ConfigFileExist, true);
+                    Config.Save(true);
+                }
+
+                RefreshConfigState();
+            }
+            catch (Exception e)
+            {
+                Error(e.Message);
+                Error(e.ToString());
+                throw;
+            }
         }
 
+        private void RefreshConfigState(ConfigurationChangedEvent configurationChangedEvent = null)
+        {
+            _enabled = Config.GetValue(Enabled);
+            _useUserScale = Config.GetValue(UseUserScale);
+            _fov = Config.GetValue(Fov);
+            _minCullingRange = Config.GetValue(MinCullingRange);
+            _maxViewRange = Config.GetValue(MaxViewRange);
+        }
 
         [HarmonyPatch(typeof(VRIKAvatar))]
         public class IkCullingPatch
         {
-            public static int IkCount = 0;
-            public static DynamicVariableSpace UserSpaceWorld;
-
             [HarmonyPrefix]
             [HarmonyPatch("OnCommonUpdate")]
             private static bool OnCommonUpdatePrefix(VRIKAvatar __instance)
             {
                 try
                 {
-                    if (!Config.GetValue(Enabled))
+                    if (!_enabled)
                     {
                         return true; //IkCulling is Disabled
                     }
-
+                    
                     if (!__instance.Enabled)
                     {
                         return false; //Ik is Disabled
@@ -93,7 +139,7 @@ namespace IkCulling
 
                     float dist = MathX.Distance(playerPos, ikPos);
 
-                    if (Config.GetValue(UseUserScale))
+                    if (_useUserScale)
                     {
                         dist = dist / __instance.LocalUserRoot.GlobalScale;
                     }
@@ -101,33 +147,17 @@ namespace IkCulling
                     float dot = MathX.Dot(dirToIk, viewDir);
 
 
-                    if (dist > Config.GetValue(MaxViewRange))
+                    if (dist > _maxViewRange)
                     {
                         return false;
                     }
 
-                    if (dist > 4 && dot < Config.GetValue(Fov))
+                    if (dist > _minCullingRange && dot < _fov)
                     {
                         return false;
                     }
 
                     return true;
-
-
-                    //IkThrottleData data = __instance.GetThrottleData();
-                    //if (data.SkippedFrames > 4)
-                    //{
-                    //    data.CurrentDeltaTime = __instance.Time.Delta;
-                    //    Traverse.Create(__instance.Time).Property("Delta").SetValue(__instance.Time.Delta + data.DeltaTimeOffset);
-                    //    data.Reset = true;
-                    //    return true;
-                    //}
-                    //else
-                    //{
-                    //    data.DeltaTimeOffset += __instance.Time.Delta;
-                    //    data.SkippedFrames++;
-                    //    return false; //Skip update
-                    //}
                 }
                 catch (Exception e)
                 {
@@ -137,54 +167,6 @@ namespace IkCulling
                     return true;
                 }
             }
-
-
-            //[HarmonyPostfix]
-            //[HarmonyPatch("SolveIK")]
-            //private static void SolveIkPostfix(VRIK __instance)
-            //{
-            //    try
-            //    {
-            //        IkThrottleData data = __instance.GetThrottleData();
-            //        if (data.Reset)
-            //        {
-            //            Traverse.Create(__instance.Time).Property("Delta").SetValue(data.CurrentDeltaTime);
-            //            data.DeltaTimeOffset = 0;
-            //            data.SkippedFrames = 0;
-            //            data.Reset = false;
-            //        }
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Error("Error in SolveIkPostfix");
-            //        Error(e.Message);
-            //        Error(e.StackTrace);
-            //        throw;
-            //    }
-            //}
-
-            //[HarmonyPostfix]
-            //[HarmonyPatch("Initiate")]
-            //static void InitiatePatch(VRIK __instance, ref Action ____solveIK)
-            //{
-            //    try
-            //    {
-            //        IkThrottleData data = __instance.AddThrottleData();
-            //        data.DeltaTimeProperty = Traverse.Create(__instance.Time).Property("Delta");
-            //        IkCount++;
-            //        data.IkIndex = IkCount;
-
-            //        data.SkippedFrames = data.IkIndex % 5; //Spread IK updates over frames
-            //        Msg("InitiatePatch IkCount:" + IkCount);
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        Error(e.Message);
-            //        Error(e.StackTrace);
-            //        throw;
-            //    }
-            //}
         }
-
     }
 }
